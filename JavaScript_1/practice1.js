2fc74e27d8a4ec6f7fe60a6250dd14d3c5771a49
wonhyuna <hyunawon417@gmail.com>
2023-01-29 18:27:26 +0900

    자바스크립트 기초 변수 선언

---
+++ JavaScript_1/practice1.js
@@ -0,0 +1,9 @@
+let espresso = 10;
+let milk = 170;
+let chocolateSyrup = 50;
+let whippedCream = 60;
+
+console.log(espresso);
+console.log(espresso + milk);
+console.log(espresso + milk + chocolateSyrup);
+console.log(espresso + milk + chocolateSyrup + whippedCream);
