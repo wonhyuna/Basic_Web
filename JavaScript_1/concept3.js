6f465952601d3481decb138782f017f3425cf00a
wonhyuna <hyunawon417@gmail.com>
2023-01-30 23:11:18 +0900

    자바스크립트 기초 불린형

---
+++ JavaScript_1/concept3.js
@@ -0,0 +1,38 @@
+//And 연산
+/*
+True AND True = True
+True AND False = False
+False AND True = False
+False AND False = False
+
+*/
+
+//OR 연산
+/*
+True OR True = True
+True OR False = True
+False OR True = True
+False OR False = False
+*/
+
+//Not 연산
+/*
+NOT True = False
+NOT False = True
+*/
+
+//불린 (True or False)
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(3 >= 2);
+console.log(3 <= 3);
+console.log(3 === 3);
+console.log(3 !== 3);
+
+console.log('Hello' === 'Hello');
+console.log('Hello' !== 'Hell');
+
+//&& || ! 연산자
+console.log(true && true);
+console.log(false || false);
+console.log(!true);
