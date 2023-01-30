d64efa571d63b7386f20282e2853e980e54c397e
wonhyuna <hyunawon417@gmail.com>
2023-01-30 22:17:58 +0900

    자바스크립트 기초 문자형

---
+++ JavaScript_1/concept2.js
@@ -0,0 +1,18 @@
+//문자열
+console.log('코드잇');
+
+//따옴표 문제
+console.log("I'm Iron man"); //다른 따옴표 사용
+console.log("He said \"I'm Iron man\""); //역슬래시 사용
+console.log('He said "I\'m Iron man"'); //역슬래시 사용
+console.log("한국 영화 역사상 아카데미상을 받은 것은 '기생충'이 처음이다.");
+console.log("아리스토텔레스는 \"인간은 사회적 동물이다.\"라고 말했다.");
+
+
+console.log(`He said "I'm Iron man"`); //백틱 사용
+console.log(`영화 '베테랑'에서 "어이가 없네~"라는 대사가 유명했다.`);
+
+//문자열 합성
+console.log('Hello' + 'Iron');
+console.log('3' + '5');
+
