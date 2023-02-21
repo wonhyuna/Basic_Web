65a600ad2a3dd6fed4f1f10e77abda8290b91cb3
wonhyuna <hyunawon417@gmail.com>
2023-02-21 22:14:30 +0900

    자바스크립트 기초 배열

---
+++ JavaScript_1/data11.js
@@ -0,0 +1,16 @@
+//배열
+let courseRanking = [
+  '자바스크립트 프로그래밍 기초',
+  'Git으로 배우는 버전 관리',
+  '컴퓨터 개론',
+  '파이썬 프로그래밍 기초'
+];
+
+//indexing(0부터 시작)
+console.log(courseRanking[2]);
+console.log(courseRanking[2 + 1]);
+
+let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
+for (let i = 0; i < 6; i++){
+  console.log(dataType[i]);
+}
