0a8215fb0eede6b53de0d801a753b39ed8eef427
wonhyuna <hyunawon417@gmail.com>
2023-02-21 20:44:09 +0900

    자바스크립트 기초 객체 예제

---
+++ JavaScript_1/data2.js
@@ -0,0 +1,12 @@
+let voca = {
+  function: '함수',
+  variable: '변수',
+  constant: '상수',
+  local: '지역의',
+  global: '전반적인'
+}
+
+console.log(voca);
+console.log(voca.local);
+console.log(voca.constant);
+console.log(voca.function);
